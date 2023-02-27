0619202512a60431cdc70e72ac93496b32926d03
Igor Amaral <igoruftm@gmail.com>
2023-02-27 11:14:26 -0300

    finalizando criacao, html e css view de autenticacao

---
+++ templates/footer.php
@@ -0,0 +1,20 @@
+<footer id="footer">
+        <div id="social-container">
+            <ul>
+                <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+            </ul>
+        </div>
+        <div id="footer-links-container">
+            <ul>
+                <li><a href="#">Adicionar Filme</a></li>
+                <li><a href="#">Adicionar Crítica</a></li>
+                <li><a href="#">Entrar / Registrar</a></li>
+            </ul>
+        </div>
+        <p>&copy; 2023 Hora de Codar</p>
+    </footer>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>

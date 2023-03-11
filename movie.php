2db23fbb68b2b9eba5c39f12c4e707b256d94a78
Igor Amaral <igoruftm@gmail.com>
2023-03-11 14:18:20 -0300

    finalizando css da view home

--- movie.php
+++ movie.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once("templates/header.php");
 
 // Verifica se o usuário está autenticado
@@ -23,6 +24,11 @@ if (empty($id)) {
     }
 }
 
+// Checar se o filme tem imagem
+if ($movie->image == "") {
+    $userData->image = "movie_cover.jpg";
+}
+
 // Checar se o filme é do próprio usuário
 $userOwnsMovie = false;
 
@@ -33,3 +39,81 @@ if (!empty($userData)) {
 }
 
 // Resgatar as reviews do filme
+
+?>
+
+<div id="main-container" class="container-fluid">
+    <div class="row">
+        <div class="offset-md-1 col-md-6 movie-container">
+            <h1 class="page-title"><?= $movie->title ?></h1>
+            <p class="movie-details">
+                <span>Duração: <?= $movie->length ?></span>
+                <span class="pipe"></span>
+                <span>Categoria: <?= $movie->category ?></span>
+                <span class="pipe"></span>
+                <span><i class="fas fa-star icone-star-padding"></i>9</span>
+            </p>
+            <iframe width="560" height="315" src="<?= $movie->trailer ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <p><?= $movie->description ?></p>
+        </div>
+        <div class="col-md-4">
+            <div class="movie-image-container" style="background-image: url('<?= $BASE_URL ?>/img/movies/<?= $movie->image ?>')"></div>
+        </div>
+        <div class="offset-md-1 col-md-10" id="reviews-container">
+            <h3 id="reviews-title">Avaliações</h3>
+            <!-- Verifica se habilita a review para o usuário ou não-->
+            <div class="col-md-12" id="review-form-container">
+                <h4>Envie sua avaliação:</h4>
+                <p class="page-description">Preencha o formulário com a nota e o comentário sobre o filme</p>
+                <form action="<?= $BASE_URL ?>review_process" id="review-form" method="POST">
+                    <input type="hidden" name="type" value="create">
+                    <input type="hidden" name="movies_id" value="<?= $movie->id ?>">
+                    <div class="form-group">
+                        <label for="rating">Nota do filme:</label>
+                        <select name="rating" id="rating" class="form-control">
+                            <option value="">Selecione</option>
+                            <option value="10">10</option>
+                            <option value="8">9</option>
+                            <option value="8">8</option>
+                            <option value="7">7</option>
+                            <option value="6">6</option>
+                            <option value="5">5</option>
+                            <option value="4">4</option>
+                            <option value="3">3</option>
+                            <option value="2">2</option>
+                            <option value="1">1</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="review">Seu comentário:</label>
+                        <textarea name="review" id="review" rows="3" class="form-control" placeholder="O que você achou do filme?"></textarea>
+                    </div>
+                    <input type="submit" class="btn card-btn padding-btn" value="Enviar comentário">
+                </form>
+            </div>
+            <!--Comentários-->
+            <div class="col-md-12 review">
+                <div class="row">
+                    <div class="col-md-1">
+                        <div class="profile-image-container review-image" style="background-image: url('<?= $BASE_URL ?>img/users/user.png')">
+                        </div>
+                    </div>
+                    <div class="col-md-9 author-details-container">
+                        <h4 class="author-name">
+                            <a href="#">Igor Amaral</a>
+                        </h4>
+                        <p><i class="fas fa-star icone-star-padding"></i>9</p>
+                    </div>
+                    <div class="col-md-12">
+                        <p class="comment-title">Comentário</p>
+                        <p>Este é o comentário do usuário</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+require_once("templates/header.php");
+?>
